d58a1c769b6c275e9f3a471b04ff7adf57e858b1
jpriestman <joe.priestman@googlemail.com>
2016-08-22 08:07:22 +0100

    morning

--- timer/models.py
+++ timer/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Batch(models.Model):
     """docstring for Batch"""
     def __str__(self):
-        return self.lot_number
+        return str(self.lot_number)
 
     lot_number = models.PositiveIntegerField(default=0)
     start_date = models.DateTimeField('date started')
