15bdf9acaaed9c915ff4441cbcfc6900844c6b3d
jpriestman <joe.priestman@googlemail.com>
2016-08-23 07:43:43 +0100

    New day 23 aug

--- timer/urls.py
+++ timer/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 app_name = 'timer'
 urlpatterns = [
-	url(r'^$',views.IndexView.as_view(), name='index'),
-# ex:/timer/batch/
-	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name ='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex:/timer/batch/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-	url(r'^(?P<lot_number>[0-9]+)/start/$', views.start, name ='start'),
+    url(r'^(?P<lot_number>[0-9]+)/start/$', views.start, name='start'),
 
-	url(r'^(?P<pk>[0-9]+)/running/$', views.RunningView.as_view(), name='running')
+    url(r'^(?P<pk>[0-9]+)/running/$', views.RunningView.as_view(), name='running'),
 
 
 
--- timer/views.py
+++ timer/views.py
@@ -7,6 +7,7 @@ from django.views import generic
 # Create your views here.
 from .models import Batch, runPeriod
 
+
 class IndexView(generic.ListView):
     template_name = 'timer/index.html'
     context_object_name = 'latest_lot_list'
@@ -15,13 +16,16 @@ class IndexView(generic.ListView):
         """Return the last five Lots"""
         return Batch.objects.order_by('-start_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Batch
     template_name = 'timer/detail.html'
 
+
 class RunningView(generic.DetailView):
     model = Batch
     template_name = 'timer/running.html'
+
 
 """def index(request):
     latest_lot_list = Batch.objects.order_by('-start_date')[:5]
@@ -33,9 +37,10 @@ def detail(request, lot_number):
     context = {'batch.id': batch.lot_number}
     return render(request, 'timer/detail.html', context)"""
 
+
 def start(request, lot_number):
     batch = get_object_or_404(Batch, lot_number=lot_number)
-    run = runPeriod(start_time = datetime.now(), batch = batch)
+    run = runPeriod(start_time=datetime.now(), batch=batch)
     return HttpResponseRedirect(reverse('timer:running', args=(batch.lot_number,)))
 
 """def running(request, lot_number):
