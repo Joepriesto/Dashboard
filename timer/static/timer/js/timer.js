5a4c44876b8c738d59c4afbeae9660ddf3cb56ec
jpriestman <joe.priestman@googlemail.com>
2016-08-26 05:48:59 +0100

    Morning 26 aug

--- timer/static/timer/js/timer.js
+++ timer/static/timer/js/timer.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 
-	window.setinterval(function(){
-		$(".panel-body").text()
-	}, 50);
+	var t = 0
+	var tM = t / 60
+	var tS = t % 60
+	var tStr = ""
+	var tStr = tStr.concat(tM, ":", tS)
+	setInterval(function(){
+		$(".panel-body").text(tStr);
+		t += 1;
+		tM = t / 60
+		tS = t % 60
+		tStr = tStr.concat(tM, ":", tS)
+
+	}, 100);
 
 
 });
