c073241bcfc08c66d0b0dce84a6977979000eacb
Joepriesto <joe.priestman@googlemail.com>
2016-08-30 17:02:12 +0100

    Added Process Model

--- timer/admin.py
+++ timer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Batch
+from .models import Batch, Process
 
 admin.site.register(Batch)
+admin.site.register(Process)
--- timer/models.py
+++ timer/models.py
@@ -5,6 +5,15 @@ from datetime import datetime, timedelta
 
 # Create your models here.
 
+class Process(models.Model):
+    """ docstring for Process """
+    def __str__(self):
+        return str(self.name)
+        
+    name = models.CharField('name', max_length=25)
+    std_hours = models.FloatField(default=0)
+    std_resource = models.PositiveIntegerField(default=1)
+    std_qty = models.PositiveIntegerField(default=1)
 
 class Batch(models.Model):
     """docstring for Batch"""
@@ -15,6 +24,7 @@ class Batch(models.Model):
     start_date = models.DateTimeField('date started', auto_now_add=True)
     start_time = models.DateTimeField('start time', auto_now_add=True)
     duration = models.DurationField('duration', default=timedelta(0))
+    process = models.ForeignKey(Process, on_delete=models.CASCADE, null=True)
 
     def get_absolute_url(self):
         return "/timer/%s/" % self.id
@@ -24,4 +34,4 @@ class Batch(models.Model):
         
 
 #     batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
-        
+
