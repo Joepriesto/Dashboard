9f189a2c565216b4e30ba95c15cc3c5ddd753ec5
Joepriesto <joe.priestman@googlemail.com>
2016-09-01 17:40:53 +0100

    unrealbrew template

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.debug import default_urlconf
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', default_urlconf),
+    url(r'^$', TemplateView.as_view(template_name="home2.html"), name='home'),
     url(r'^timer/', include('timer.urls')),
 ]
---
+++ Dashboard/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import HttpResponseRedirect
+from datetime import datetime, timedelta
+from django.urls import reverse
+from django.views import generic
+
+# Create your views here.
+# from .models import Batch
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+
+# class CreateView(generic.CreateView):
+#     model = Batch
+#     fields = ['lot_number']
+#     #success_url = 'timer/detail'
+
+
+#     # def post(self, request, *args, **kwargs):
+#     #     self.object.start_date = datetime.now()
+#     #     print(self.get_form())
+#     #     return super(CreateView, self).post(request, *args, **kwargs)
+
+
+# class DetailView(generic.DetailView):
+#     model = Batch
+#     template_name = 'timer/detail.html'
+
+
+# class RunningView(generic.DetailView):
+#     model = Batch
+#     template_name = 'timer/running.html'
+#     duration = timedelta()
+
+#     def get_object(self):
+#         object = super(RunningView, self).get_object()
+
+#         object.duration = datetime.now() - object.start_time.replace(tzinfo=None)
+#         object.save()
+#         return object
+
+
+# """def index(request):
+#     latest_lot_list = Batch.objects.order_by('-start_date')[:5]
+#     context = {'latest_lot_list': latest_lot_list}
+#     return render(request, 'timer/index.html', context)
+
+# def detail(request, lot_number):
+#     batch = get_object_or_404(Batch, lot_number=lot_number)
+#     context = {'batch.id': batch.lot_number}
+#     return render(request, 'timer/detail.html', context)"""
+
+
+# def start(request, lot_number):
+#     batch = get_object_or_404(Batch, lot_number=lot_number)
+#     if batch.start_time == 0:
+#         batch.start_time=datetime.now()
+#         batch.save()
+
+#     return HttpResponseRedirect(reverse('timer:running', args=(batch.id,)))
+
+# """def running(request, lot_number):
+#     batch = get_object_or_404(Batch, lot_number=lot_number)
+#     return render(request, 'polls/results.html', {'batch.id':batch.lot_number})
+# """
